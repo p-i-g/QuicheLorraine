b30b73362d449fb22bd864b93cbe28e26fc80edc
User <reachkabirjain@gmail.com>
2023-11-11 03:15:40 +0800

    AI Works

---
+++ app/ai.py
@@ -0,0 +1,45 @@
+import openai
+from langchain.chains import RetrievalQA
+from langchain.llms.ollama import Ollama
+from langchain.prompts import PromptTemplate
+from openai import OpenAI
+from transformers import pipeline
+
+def get_openai_response(prompt):
+    # Set your OpenAI API key
+    client = OpenAI(
+        # defaults to os.environ.get("OPENAI_API_KEY")
+        api_key="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",
+    )
+
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": prompt,
+            }
+        ],
+        model="gpt-3.5-turbo",
+    )
+
+    return chat_completion.choices[0].message.content
+
+
+def get_output(s):
+    classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
+    candidate_labels = ['electrodynamics', 'News', 'Emails', 'Legal', 'Medical', 'Fiction', 'Business',
+                        'Advertisements', 'Government', 'Others']
+    classifier1 = pipeline("sentiment-analysis", model="michellejieli/inappropriate_text_classifier")
+
+    print(classifier1(s)[0]['label'])
+    if classifier1(s)[0]['label'] == "NSFW":
+        return "Please do not send inappropriate messages"
+    if classifier(s, candidate_labels)['labels'][0] != "electrodynamics":
+        return "Please only send questions related to electrodynamics"
+    return get_openai_response(s)
+
+
+
+
+if __name__ == '__main__':
+    print(get_output("What is bremsstrahlung?"))
